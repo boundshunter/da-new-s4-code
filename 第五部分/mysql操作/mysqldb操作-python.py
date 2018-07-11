c491ac2b5e19ee60b014198e15e5439f4f15e7ca
sujunfeng <76686094@qq.com>
2018-07-11 18:19:13 +0800

    Default

--- 第五部分/mysql操作/mysqldb操作-python.py
+++ 第五部分/mysql操作/mysqldb操作-python.py
@@ -5,3 +5,18 @@ __author__ = 'jfsu'
 # cp /usr/lib64/python3.6/configparser.py /usr/lib64/python3.6/ConfigParser.py 否则系统找不到
 
 # 之后才能执行 pip3.6 install mysql-python
+
+#mysql2.x使用
+import MySQLdb
+
+conn = MySQLdb.connect(host='10.0.1.110',user='jfsu',passwd='gxw#mP8t',db='test')
+
+cur = conn.cursor()
+
+recount = cur.execute('select * from B')
+
+conn.commit()
+
+cur.close()
+conn.close()
+print(recount)
